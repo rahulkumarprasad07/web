d90190cf2ce38f7b46d880529eab0327bb932d1a
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-15 21:00:19 +0530

    adding

--- REACT-FILES/react-four/src/components/Counter.jsx
+++ REACT-FILES/react-four/src/components/Counter.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Counter.css'
 
 const Counter = () => {
+  const [count,setCount]=useState(0);
+
   return (
     <div className='counter'>
-        <p id='para'>you have clicked 0 times</p>
-        <button id='hit'>hit me</button>
+        <p id='para'>you have clicked {count} times</p>
+        <button id='hit' onClick={()=>{
+          setCount(count+1) }}>hit me</button>
     </div>
   )
 }
